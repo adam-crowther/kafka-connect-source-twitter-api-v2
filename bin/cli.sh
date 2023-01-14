7cf89d4e85cbf9c8410de95c31c9a5c527ef0238
Adam Crowther <github@adamcc.ch>
2023-01-14 19:20:25 +0100

    Removed the absolute path

--- bin/cli.sh
+++ bin/cli.sh
@@ -1,6 +1,7 @@
+PWD=$(pwd)
 winpty docker run --rm -it \
-                  -v //c/dev/kafka-connect-twitter-api-v2/build/config:/kafka-connect-source-twitter-api-v2/config \
-                  -v //c/dev/kafka-connect-twitter-api-v2/offsets:/kafka-connect-source-twitter-api-v2/offsets \
+                  -v /${PWD}/build/config:/kafka-connect-source-twitter-api-v2/config \
+                  -v /${PWD}/offsets:/kafka-connect-source-twitter-api-v2/offsets \
                   --net=kafka-connect-twitter-api-v2_app-tier \
                   adamcc/kafka-connect-source-twitter-api-v2:0.1 \
                   bash
